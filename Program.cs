370ab598acb6090e530d111324eb0a8a3eff1ef0
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-07 19:01:59 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> users = new Dictionary<string, string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] parts = Console.ReadLine().Split();
+                string command = parts[0];
+
+                if (command == "register")
+                {
+                    string username = parts[1];
+                    string licensePlateNumber = parts[2];
+
+                    if (users.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    else
+                    {
+                        users.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    }
+                }
+                else
+                {
+                    string username = parts[1];
+                    bool removed = users.Remove(username);  //returns true if removed, false if not removed, not found
+
+                    if (removed)  // if removed == true 
+                    {
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                    else // id removed == false, not removed, not found
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                }
+            }
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+    }
+}
